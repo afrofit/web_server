cd4a93df7b811ba33edf38c976cb8100588e7045
paddyodebiyi <paddy.o.dev@gmail.com>
2022-08-28 16:16:18 +0100

    Fixed the start script in package.json

--- src/data-source.ts
+++ src/data-source.ts
@@ -1,9 +1,26 @@
 import "reflect-metadata";
 import { DataSource } from "typeorm";
 
+// export const AppDataSource = new DataSource({
+//   type: "mongodb",
+//   database: "afrofit",
+//   synchronize: false,
+//   logging: false,
+//   entities: ["build/entity/**/*.js"],
+//   migrations: ["build/migrations/**/*.js"],
+//   subscribers: ["build/subscribers/**/*.js"],
+//   port: 27017,
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+//   // dropSchema: true,
+// });
+
 export const AppDataSource = new DataSource({
   type: "mongodb",
-  database: "afrofit",
+  database: process.env.MONGO_DATABASE,
+  host: process.env.MONGO_HOST,
+  username: process.env.MONGO_USERNAME,
+  password: process.env.MONGO_PASSWORD,
   synchronize: false,
   logging: false,
   entities: ["build/entity/**/*.js"],
@@ -12,5 +29,6 @@ export const AppDataSource = new DataSource({
   port: 27017,
   useNewUrlParser: true,
   useUnifiedTopology: true,
+
   // dropSchema: true,
 });
