75f32cd2ad437338239dd7c97caf13732d8cea80
paddyodebiyi <paddy.o.dev@gmail.com>
2022-08-28 14:34:04 +0100

    Added in password reset routes

--- src/controllers/user/index.ts
+++ src/controllers/user/index.ts
@@ -2,3 +2,5 @@ export { default as createUser } from "./create-user";
 export { default as loginUser } from "./login-user";
 export { default as updateUserDp } from "./update-user-dp";
 export { default as getUserSubscription } from "./get-user-subscription";
+export { default as setNewPassword } from "./set-new-password";
+export { default as sendPasswordResetLink } from "./send-password-reset-link";
--- src/routes/user.ts
+++ src/routes/user.ts
@@ -3,6 +3,8 @@ import {
   createUser,
   getUserSubscription,
   loginUser,
+  sendPasswordResetLink,
+  setNewPassword,
   updateUserDp,
 } from "../controllers/user";
 
@@ -12,5 +14,7 @@ router.post("/create", createUser);
 router.post("/login", loginUser);
 router.post("/update-dp/:userId", updateUserDp);
 router.post("/subscription/:userId", getUserSubscription);
+router.post("/send-password-reset-link", sendPasswordResetLink);
+router.post("/set-new-password/:userId", setNewPassword);
 
 export { router as userRoutes };
