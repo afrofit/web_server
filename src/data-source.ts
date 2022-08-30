8f2f6ba461a92307e47df1e5ff4ddb7cb05f0a1c
paddyodebiyi <paddy.o.dev@gmail.com>
2022-08-30 10:40:56 +0100

    Cleanup

--- src/data-source.ts
+++ src/data-source.ts
@@ -1,35 +1,14 @@
 import "reflect-metadata";
 import { DataSource } from "typeorm";
 
-// export const AppDataSource = new DataSource({
-//   type: "mongodb",
-//   database: "afrofit",
-//   synchronize: false,
-//   logging: false,
-//   entities: ["build/entity/**/*.js"],
-//   migrations: ["build/migrations/**/*.js"],
-//   subscribers: ["build/subscribers/**/*.js"],
-//   port: 27017,
-//   useNewUrlParser: true,
-//   useUnifiedTopology: true,
-//   // dropSchema: true,
-// });
-
 export const AppDataSource = new DataSource({
   type: "mongodb",
   url: process.env.MONGO_URL,
-  // database: process.env.MONGO_DATABASE,
-  // host: process.env.MONGO_HOST,
-  // username: process.env.MONGO_USERNAME,
-  // password: process.env.MONGO_PASSWORD,
   synchronize: false,
   logging: false,
   entities: ["build/entity/**/*.js"],
   migrations: ["build/migrations/**/*.js"],
   subscribers: ["build/subscribers/**/*.js"],
-  // port: 27017,
   useNewUrlParser: true,
   useUnifiedTopology: true,
-
-  // dropSchema: true,
 });
